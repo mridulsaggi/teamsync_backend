ff360a7a8d86e808e5a13e3d5e5698da0ee63475
mridul saggi <91930535+mridulsaggi@users.noreply.github.com>
2024-04-11 23:25:45 +0530

    initial commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app=express();
 dotenv.config();
 // implementing cors
 const corsoptions={
-    origin:"https://localhost/3000",
+    origin:"http://localhost/3000",
     methods:"*",
     credentials:true
 }
