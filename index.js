ba586182bedd858c70673241c28571cff1e9975b
mridul saggi <91930535+mridulsaggi@users.noreply.github.com>
2024-04-12 22:40:37 +0530

    updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app=express();
 dotenv.config();
 // implementing cors
 const corsoptions={
-    origin:"http://localhost:3000",
+    origin:"http://localhost:5173",
     methods:"*",
     credentials:true
 }
